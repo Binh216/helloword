854eff09ec22464932896ea312e62bb82af58426
Binh216 <50403101+Binh216@users.noreply.github.com>
2023-02-19 21:29:35 +0700

    first commit

---
+++ src/webparts/helloword/components/IHellowordProps.ts
@@ -0,0 +1,7 @@
+export interface IHellowordProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/helloword/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface IHellowordWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'HellowordWebPartStrings' {
+  const strings: IHellowordWebPartStrings;
+  export = strings;
+}
